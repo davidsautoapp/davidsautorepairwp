75a66ea41965184e86c14c2168c261af0782ea40
Ben Dah <art.wyse11@gmail.com>
2021-05-25 11:42:48 -0400

    Hichcock-Ben theme working

---
+++ wp-app/wp-content/themes/hitchcock/footer.php
@@ -0,0 +1,11 @@
+		</main><!-- #site-content -->
+
+		<div class="credits section-inner">
+			<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a></p>
+		</div><!-- .credits -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+	
+</html>
